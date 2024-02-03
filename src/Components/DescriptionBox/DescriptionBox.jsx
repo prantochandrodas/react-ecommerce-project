80fa24830be52d1d240de50e50614c58a1ab4bfc
pranto <prantochandrodas@gmail.com>
2024-02-03 13:20:48 +0600

    add descriptionBox

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">
+                    Description
+                </div>
+                <div className="descriptionbox-nav-box fade">
+                    Reviews (122)
+                </div>
+                
+            </div>
+            <div className="descriptionbox-description">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis optio corporis assumenda odio libero debitis minus, recusandae reiciendis cum perspiciatis maiores aliquid quibusdam nesciunt numquam animi nisi ratione in ut. Quia aliquam deserunt voluptate repellendus excepturi dolores. Nihil incidunt, laudantium error doloremque aliquid dolorum consectetur quos, dicta eligendi minima atque dolore minus vel? Iusto ullam corrupti fugiat sunt architecto nam maiores perferendis qui, harum consequatur? Dignissimos vitae consequuntur asperiores facilis architecto sint, corrupti veniam doloribus cumque! Ex laudantium commodi adipisci delectus nisi? Impedit alias nemo ut, numquam quaerat nobis? Illo eveniet laboriosam harum aliquam quas perspiciatis nulla officia enim eligendi!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit repellendus officiis facilis reiciendis totam? Tempore dignissimos sequi molestias cumque architecto quisquam expedita harum ab consequuntur sed? Expedita quisquam officiis quos vel deleniti, impedit inventore necessitatibus ipsum nisi quod modi mollitia accusantium eaque voluptatem quaerat architecto ducimus quidem debitis in consequatur! Laudantium illo, assumenda natus quam explicabo amet asperiores aut recusandae dignissimos blanditiis officia ratione, quasi obcaecati, aliquid autem! Reiciendis molestias tempora doloremque rem, voluptatibus consequuntur autem qui fuga, enim sit repellendus deleniti, eligendi quidem? Inventore iusto nam maiores nobis fugit quaerat sed? Perferendis dolore temporibus nostrum quia! Ipsum, corporis ab?</p>
+            </div>
+        </div>
+    );
+};
+
+export default DescriptionBox;
